5e0479fe5ac35141659b02165b8fb816b812ff4d
Alduin <Alduin@DESKTOP-KFNDSM0>
2023-04-14 21:17:03 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net;
+
+class currency
+{
+    static void ReadAPI(string[] args)
+    {
+        using var client = new WebClient();
+        var content = client.DownloadString("https://api.kucoin.com/api/v1/market/stats?symbol=BTC-USDT");
+        Console.WriteLine(content);
+    }
+}
